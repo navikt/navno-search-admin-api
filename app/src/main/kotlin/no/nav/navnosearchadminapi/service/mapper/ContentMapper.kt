b283806842518eebe0ab4aee388c1780eedea202
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-10-31 14:07:06 +0100

    Splittet app i to moduler - app og lib (fellesbibliotek)

---
+++ app/src/main/kotlin/no/nav/navnosearchadminapi/service/mapper/ContentMapper.kt
@@ -0,0 +1,59 @@
+package no.nav.navnosearchadminapi.service.mapper
+
+import no.nav.navnosearchadminapi.common.model.ContentDao
+import no.nav.navnosearchadminapi.common.model.MultiLangField
+import no.nav.navnosearchadminapi.common.utils.ENGLISH
+import no.nav.navnosearchadminapi.common.utils.NORWEGIAN
+import no.nav.navnosearchadminapi.common.utils.NORWEGIAN_BOKMAAL
+import no.nav.navnosearchadminapi.common.utils.norwegianLanguageCodes
+import no.nav.navnosearchadminapi.common.utils.supportedLanguages
+import no.nav.navnosearchadminapi.dto.inbound.ContentDto
+import no.nav.navnosearchadminapi.service.validation.enums.ValidMetatags
+import no.nav.navnosearchadminapi.utils.createInternalId
+import org.springframework.data.elasticsearch.core.suggest.Completion
+import org.springframework.stereotype.Component
+
+@Component
+class ContentMapper {
+    fun toContentDao(content: ContentDto, teamName: String): ContentDao {
+        return ContentDao(
+            id = createInternalId(teamName, content.id!!),
+            teamOwnedBy = teamName,
+            href = content.href!!,
+            autocomplete = Completion(listOf(content.title)),
+            title = toMultiLangField(content.title!!, content.metadata!!.language!!),
+            ingress = toMultiLangField(content.ingress!!, content.metadata.language!!),
+            text = toMultiLangField(content.text!!, content.metadata.language),
+            createdAt = content.metadata.createdAt!!,
+            lastUpdated = content.metadata.lastUpdated!!,
+            audience = content.metadata.audience!!,
+            language = resolveLanguage(content.metadata.language),
+            isFile = content.metadata.isFile,
+            fylke = content.metadata.fylke,
+            metatags = resolveMetatags(content.metadata.metatags, content.metadata.fylke, content.metadata.isFile),
+            keywords = content.metadata.keywords,
+        )
+    }
+
+    fun resolveMetatags(metatags: List<String>, fylke: String?, isFile: Boolean): List<String> {
+        if (metatags.isEmpty() && fylke == null && !isFile) {
+            return listOf(ValidMetatags.INFORMASJON.descriptor)
+        }
+        return metatags
+    }
+
+    fun resolveLanguage(language: String): String {
+        if (language.equals(NORWEGIAN, ignoreCase = true)) {
+            return NORWEGIAN_BOKMAAL
+        }
+        return language.lowercase()
+    }
+
+    fun toMultiLangField(value: String, language: String): MultiLangField {
+        return MultiLangField(
+            en = if (ENGLISH == language) value else null,
+            no = if (norwegianLanguageCodes.contains(language)) value else null,
+            other = if (!supportedLanguages.contains(language)) value else null,
+        )
+    }
+}
---
+++ app/src/test/kotlin/no/nav/navnosearchadminapi/AbstractIntegrationTest.kt
@@ -0,0 +1,61 @@
+package no.nav.navnosearchadminapi
+
+import no.nav.navnosearchadminapi.common.repository.ContentRepository
+import no.nav.navnosearchadminapi.utils.initialTestData
+import org.junit.jupiter.api.extension.ExtendWith
+import org.opensearch.testcontainers.OpensearchContainer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.util.TestPropertyValues
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.time.Duration
+
+@Testcontainers(disabledWithoutDocker = true, parallel = true)
+@ContextConfiguration(initializers = [AbstractIntegrationTest.Initializer::class])
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension::class)
+@AutoConfigureWireMock(port = 0)
+abstract class AbstractIntegrationTest {
+
+    @Autowired
+    lateinit var restTemplate: TestRestTemplate
+
+    @Autowired
+    lateinit var repository: ContentRepository
+
+    @LocalServerPort
+    var serverPort: Int? = null
+
+    fun host() = "http://localhost:$serverPort"
+
+    fun indexCount() = repository.count()
+
+    fun setupIndex() {
+        repository.deleteAll()
+        repository.saveAll(initialTestData)
+    }
+
+    internal class Initializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+        override fun initialize(configurableApplicationContext: ConfigurableApplicationContext) {
+            TestPropertyValues.of("opensearch.uris=" + opensearch.getHttpHostAddress())
+                .applyTo(configurableApplicationContext.environment)
+        }
+    }
+
+    companion object {
+        @Container
+        val opensearch: OpensearchContainer = OpensearchContainer("opensearchproject/opensearch:2.0.0")
+            .withStartupAttempts(5)
+            .withStartupTimeout(Duration.ofMinutes(2))
+    }
+}
