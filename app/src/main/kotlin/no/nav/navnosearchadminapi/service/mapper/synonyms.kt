8576408e545eab4fc57fe3cae6df035491c0a911
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-01-11 15:39:30 +0100

    Lagt til flere synonymer

--- app/src/main/kotlin/no/nav/navnosearchadminapi/service/mapper/synonyms.kt
+++ app/src/main/kotlin/no/nav/navnosearchadminapi/service/mapper/synonyms.kt
@@ -2,5 +2,9 @@ package no.nav.navnosearchadminapi.service.mapper
 
 val synonyms = mapOf(
     "alderspensjon" to listOf("pensjon"),
-    "utbetalingsdato" to listOf("utbetaling")
+    "utbetalingsdato" to listOf("utbetaling"),
+    "barnebidrag" to listOf("bidrag"),
+    "uføretrygd" to listOf("ufør"),
+    "utbetalingsdato" to listOf("utbetaling"),
+    "saksbehandlingstider" to listOf("saksbehandling"),
 )
