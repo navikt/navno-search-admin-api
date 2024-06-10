d0cf3c8020bb0ebe2c029fc39959c6b8aeca5f0c
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-06-10 10:27:41 +0200

    Bumps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     val kotlinVersion = "1.9.23"
-    val springBootVersion = "3.2.5"
+    val springBootVersion = "3.3.0"
     val springDepMgmtVersion = "1.1.4"
     val versionsVersion = "0.51.0"
 
@@ -26,7 +26,7 @@ repositories {
 dependencies {
     val navSecurityVersion = "4.1.4"
     val logstashVersion = "7.4"
-    val opensearchVersion = "1.3.0"
+    val opensearchVersion = "1.5.0"
     val jsoupVersion = "1.17.2"
     val opensearchTestcontainersVersion = "2.0.1"
     val testcontainersVersion = "1.19.7"
