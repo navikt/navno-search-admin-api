cc1a74840dd4af2653942f78db9d7d833c95b102
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-10-22 14:32:17 +0200

    Kotest i validator-tester

--- app/src/test/kotlin/no/nav/navnosearchadminapi/validation/ContentDtoValidatorTest.kt
+++ app/src/test/kotlin/no/nav/navnosearchadminapi/validation/ContentDtoValidatorTest.kt
@@ -1,5 +1,10 @@
 package no.nav.navnosearchadminapi.validation
 
+import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.collections.shouldContainOnly
+import io.kotest.matchers.maps.shouldBeEmpty
+import io.kotest.matchers.maps.shouldHaveKey
+import io.kotest.matchers.maps.shouldHaveSize
 import no.nav.navnosearchadminapi.common.enums.ValidAudiences
 import no.nav.navnosearchadminapi.common.enums.ValidFylker
 import no.nav.navnosearchadminapi.common.enums.ValidMetatags
@@ -9,7 +14,6 @@ import no.nav.navnosearchadminapi.service.validation.ContentDtoValidator
 import no.nav.navnosearchadminapi.utils.dummyContentDto
 import no.nav.navnosearchadminapi.utils.enumDescriptors
 import no.nav.navnosearchadminapi.utils.mockedKodeverkResponse
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -34,7 +38,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
     fun testValidation() {
         val content = listOf(dummyContentDto())
         val validationErrors = validator.validate(content)
-        assertThat(validationErrors).isEmpty()
+
+        validationErrors.shouldBeEmpty()
     }
 
     @Test
@@ -42,7 +47,7 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(audience = emptyList()))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).isEmpty()
+        validationErrors.shouldBeEmpty()
     }
 
     @Test
@@ -50,9 +55,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(text = null))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Påkrevd felt mangler: text")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Påkrevd felt mangler: text")
     }
 
     @Test
@@ -60,9 +64,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(audience = listOf(invalidValue)))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Ugyldig verdi for metadata.audience: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidAudiences>()}")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Ugyldig verdi for metadata.audience: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidAudiences>()}")
     }
 
     @Test
@@ -70,9 +73,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(type = invalidValue))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Ugyldig verdi for metadata.type: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidTypes>()}")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Ugyldig verdi for metadata.type: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidTypes>()}")
     }
 
     @Test
@@ -80,9 +82,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(fylke = invalidValue))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Ugyldig verdi for metadata.fylke: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidFylker>()}")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Ugyldig verdi for metadata.fylke: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidFylker>()}")
     }
 
     @Test
@@ -90,9 +91,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(metatags = listOf(invalidValue)))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Ugyldig verdi for metadata.metatags: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidMetatags>()}")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Ugyldig verdi for metadata.metatags: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidMetatags>()}")
     }
 
     @Test
@@ -100,9 +100,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(language = invalidValue))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Ugyldig verdi for metadata.language: invalidValue. Må være tobokstavs språkkode fra kodeverk-api.")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Ugyldig verdi for metadata.language: invalidValue. Må være tobokstavs språkkode fra kodeverk-api.")
     }
 
     @Test
@@ -110,9 +109,8 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         val content = listOf(dummyContentDto(languageRefs = listOf(invalidValue)))
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(1)
-        assertThat(validationErrors[id]).hasSize(1)
-        assertThat(validationErrors[id]!!.first()).isEqualTo("Ugyldig verdi for metadata.languageRefs: invalidValue. Må være tobokstavs språkkode fra kodeverk-api.")
+        validationErrors shouldHaveSize 1
+        validationErrors[id]!!.shouldContainOnly("Ugyldig verdi for metadata.languageRefs: invalidValue. Må være tobokstavs språkkode fra kodeverk-api.")
     }
 
     @Test
@@ -126,11 +124,15 @@ class ContentDtoValidatorTest(@Mock val kodeverkConsumer: KodeverkConsumer) {
         )
         val validationErrors = validator.validate(content)
 
-        assertThat(validationErrors).hasSize(2)
-        assertThat(validationErrors[firstId]).hasSize(1)
-        assertThat(validationErrors[secondId]).hasSize(2)
-        assertThat(validationErrors[firstId]!!.first()).isEqualTo("Ugyldig verdi for metadata.language: invalidValue. Må være tobokstavs språkkode fra kodeverk-api.")
-        assertThat(validationErrors[secondId]!!).contains("Ugyldig verdi for metadata.audience: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidAudiences>()}")
-        assertThat(validationErrors[secondId]!!).contains("Ugyldig verdi for metadata.fylke: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidFylker>()}")
+        validationErrors shouldHaveSize 2
+
+        validationErrors.shouldHaveKey(firstId)
+        validationErrors[firstId]!!.shouldContainOnly("Ugyldig verdi for metadata.language: invalidValue. Må være tobokstavs språkkode fra kodeverk-api.")
+
+        validationErrors.shouldHaveKey(secondId)
+        validationErrors[secondId]!! shouldContainExactly (listOf(
+            "Ugyldig verdi for metadata.audience: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidAudiences>()}",
+            "Ugyldig verdi for metadata.fylke: $invalidValue. Gyldige verdier: ${enumDescriptors<ValidFylker>()}"
+        ))
     }
 }
