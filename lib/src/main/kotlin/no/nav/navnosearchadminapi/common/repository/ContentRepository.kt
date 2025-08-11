4698451780b193f64c58ef550642b9db30223f85
Tale Prestmo <tale.prestmo@gmail.com>
2025-08-11 09:09:06 +0200

    remove ref to unused methos

--- lib/src/main/kotlin/no/nav/navnosearchadminapi/common/repository/ContentRepository.kt
+++ lib/src/main/kotlin/no/nav/navnosearchadminapi/common/repository/ContentRepository.kt
@@ -1,10 +1,7 @@
 package no.nav.navnosearchadminapi.common.repository
 
 import no.nav.navnosearchadminapi.common.model.Content
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 
 interface ContentRepository : CrudRepository<Content, String> {
-    fun findAllByTeamOwnedBy(teamOwnedBy: String, pageable: Pageable): Page<Content>
 }
