5834ff9c1ecb44a45ca6730f277b47e0be8c7ba4
Tale Prestmo <tale.prestmo@gmail.com>
2025-08-11 13:59:06 +0200

    delete repo from lib

--- lib/src/main/kotlin/no/nav/navnosearchadminapi/common/repository/ContentRepository.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.navnosearchadminapi.common.repository
-
-import no.nav.navnosearchadminapi.common.model.Content
-import org.springframework.data.repository.CrudRepository
-
-interface ContentRepositoryLib : CrudRepository<Content, String> {
-}
