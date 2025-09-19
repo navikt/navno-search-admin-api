884a621d4f215f7f3bb65eb474e61396bd1ccdd8
Tania Holst <holst.tania@gmail.com>
2025-09-19 08:51:43 +0200

    bump

--- app/src/test/kotlin/no/nav/navnosearchadminapi/integrationtests/config/OpensearchConfig.kt
+++ app/src/test/kotlin/no/nav/navnosearchadminapi/integrationtests/config/OpensearchConfig.kt
@@ -1,15 +1,19 @@
 package no.nav.navnosearchadminapi.integrationtests.config
 
-import org.opensearch.testcontainers.OpensearchContainer
+import org.opensearch.testcontainers.OpenSearchContainer
 import org.springframework.boot.test.context.TestConfiguration
+import org.testcontainers.utility.DockerImageName
 import java.time.Duration
 
 @TestConfiguration
 class OpensearchConfig {
     companion object {
-        private val opensearch: OpensearchContainer<*> = OpensearchContainer("opensearchproject/opensearch:2.11.1")
-            .withStartupAttempts(5)
-            .withStartupTimeout(Duration.ofMinutes(2))
+        private val opensearch: OpenSearchContainer<*> =
+            OpenSearchContainer(
+                DockerImageName.parse("opensearchproject/opensearch:2.11.1")
+            )
+                .withStartupAttempts(5)
+                .withStartupTimeout(Duration.ofMinutes(2))
 
         init {
             opensearch.start()
