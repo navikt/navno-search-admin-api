748d09f00aa53a4608aa0dc06e30793c414225bc
Tania Holst <holst.tania@gmail.com>
2026-02-03 12:41:58 +0100

    :recycle: Refaktorerte build.gradle.kts-filer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
-    kotlin("jvm") version "2.2.21" apply false
-    kotlin("plugin.spring") version "2.2.21" apply false
+    val kotlinVersion = "2.2.21"
+    val springBootVersion = "3.5.7"
+    val springDepMgmtVersion = "1.1.7"
+    val versionsVersion = "0.53.0"
 
-    id("org.springframework.boot") version "3.5.7" apply false
-    id("io.spring.dependency-management") version "1.1.7" apply false
+    kotlin("jvm") version kotlinVersion apply false
+    kotlin("plugin.spring") version kotlinVersion apply false
 
-    id("com.github.ben-manes.versions") version "0.53.0" apply false
+    id("org.springframework.boot") version springBootVersion apply false
+    id("io.spring.dependency-management") version springDepMgmtVersion apply false
+    id("com.github.ben-manes.versions") version versionsVersion apply false
 }
 
 allprojects {
