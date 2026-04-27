83557acf9099871594ddf747f79c070a82a467dc
Tania Holst <holst.tania@gmail.com>
2026-04-27 14:29:24 +0200

    Revert OpenSearch auto-config exclusions and fix health probe 503 by disabling SSL health indicator and switching probes to dedicated   liveness/readiness paths

--- app/src/main/kotlin/no/nav/navnosearchadminapi/NavnoSearchAdminApiApplication.kt
+++ app/src/main/kotlin/no/nav/navnosearchadminapi/NavnoSearchAdminApiApplication.kt
@@ -1,16 +1,9 @@
 package no.nav.navnosearchadminapi
 
-import org.opensearch.spring.boot.autoconfigure.OpenSearchRestHighLevelClientAutoConfiguration
-import org.opensearch.spring.boot.autoconfigure.ReactiveOpenSearchClientAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [
-        OpenSearchRestHighLevelClientAutoConfiguration::class,
-        ReactiveOpenSearchClientAutoConfiguration::class,
-    ]
-)
+@SpringBootApplication
 class NavnoSearchAdminApiApplication
 
 fun main(args: Array<String>) {
