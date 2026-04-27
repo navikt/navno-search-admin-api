e7fad2e8630a4138caf99429113dc889933452e9
Tania Holst <holst.tania@gmail.com>
2026-04-27 14:16:58 +0200

    Fix health probe 503 by disabling SSL health indicator, excluding unused OpenSearch auto-configurations, and switching nais.yaml probes to dedicated liveness/readiness paths

--- app/src/main/kotlin/no/nav/navnosearchadminapi/NavnoSearchAdminApiApplication.kt
+++ app/src/main/kotlin/no/nav/navnosearchadminapi/NavnoSearchAdminApiApplication.kt
@@ -1,9 +1,16 @@
 package no.nav.navnosearchadminapi
 
+import org.opensearch.spring.boot.autoconfigure.OpenSearchRestHighLevelClientAutoConfiguration
+import org.opensearch.spring.boot.autoconfigure.ReactiveOpenSearchClientAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        OpenSearchRestHighLevelClientAutoConfiguration::class,
+        ReactiveOpenSearchClientAutoConfiguration::class,
+    ]
+)
 class NavnoSearchAdminApiApplication
 
 fun main(args: Array<String>) {
